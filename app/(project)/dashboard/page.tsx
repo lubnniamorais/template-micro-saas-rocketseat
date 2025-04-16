7951c410759c40b301dd27d703d3577f3b97c37d
lubnniamorais <lubnnia@gmail.com>
2025-04-16 11:11:06 -0300

    feat: Add integration with Firebase database

--- app/(project)/dashboard/page.tsx
+++ app/(project)/dashboard/page.tsx
@@ -1,21 +1,34 @@
-import { auth } from "@/app/lib/auth";
-import { redirect } from "next/navigation";
+import { handleAuth } from '@/app/actions/handle-auth';
+import { auth } from '@/app/lib/auth';
+import { redirect } from 'next/navigation';
 
 export default async function Dashboard() {
   // Estamos no lado do servidor
   const session = await auth();
 
-  if(!session) {
+  if (!session) {
     redirect('/login');
   }
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold">Protected Dashboard</h1>
+    <div className='flex flex-col gap-10 items-center justify-center h-screen'>
+      <h1 className='text-4xl font-bold'>Protected Dashboard</h1>
 
-      <p>{session?.user?.email ? session?.user?.email : "Usuário não está logado!"}</p>
+      <p>
+        {session?.user?.email
+          ? session?.user?.email
+          : 'Usuário não está logado!'}
+      </p>
+      {session.user?.email && (
+        <form action={handleAuth}>
+          <button
+            type='submit'
+            className='border rounded-md px-2 cursor-pointer'
+          >
+            Logout
+          </button>
+        </form>
+      )}
     </div>
   );
 }
-
-
